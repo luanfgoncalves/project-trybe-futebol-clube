8f007544ba784f13c1b8a4d534e274d7cec51b24
luanfgoncalves <luanfgoncalves@outlook.com>
2023-01-26 13:17:43 -0300

    FEAT: validação com resposta identificadora de erro(Incorrect email or password)

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+
 import authenticateToken from '../services/userService';
 
 // verifica os tokens via jwt - chamado em loginRouter
@@ -8,6 +9,12 @@ const loginAuth = async (req:Request, res:Response, next:NextFunction) => {
 
     const expectedToken = await authenticateToken({ email, password });
 
+    // if checa a resposta o jwt
+    if (expectedToken.type === 'USER_NOT_FOUND') {
+      return res
+        .status(401)
+        .json({ message: 'Incorrect email or password' });
+    }
     return res.status(200).json({ token: expectedToken });
   } catch (err) {
     next(err);
--- app/backend/src/services/userService.ts
+++ app/backend/src/services/userService.ts
@@ -3,11 +3,16 @@ import User from '../database/models/userModel';
 import createToken from '../Middlewares/jwtAuthentication';
 
 type Login = {
-  email: string,
+  email:string,
   password:string,
 };
 
-const authenticateToken = async ({ email, password }:Login) => {
+type ResType = {
+  type?:'USER_NOT_FOUND',
+  token?:string,
+}
+
+const authenticateToken = async ({ email, password }:Login): Promise<ResType> => {
   const user = await User.findOne({
     where: { email },
   });
@@ -15,12 +20,12 @@ const authenticateToken = async ({ email, password }:Login) => {
   if (!user) return { type: 'USER_NOT_FOUND' };
 
   // compareSync https://snyk.io/advisor/npm-package/bcrypt/functions/bcrypt.compareSync
-  const samePassword = bcrypt.compareSync(password, user.password);
+  const isPassEqual = bcrypt.compareSync(password, user.password);
 
-  if (!samePassword) return { type: 'USER_NOT_FOUND' };
+  if (!isPassEqual) return { type: 'USER_NOT_FOUND' };
 
   const tokenCreated = createToken(email);
-  return tokenCreated;
+  return {token: tokenCreated };
 };
 
 export default authenticateToken;
